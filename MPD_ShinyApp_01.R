742270557192db82d494830d8bc4c14da9812eab
SG540 <65647060+SG540@users.noreply.github.com>
2021-09-15 12:46:54 +0900

    Update MPD_ShinyApp_01.R
    fixing SQL connection

--- MPD_ShinyApp_01.R
+++ MPD_ShinyApp_01.R
@@ -30,12 +30,19 @@ library(dbplyr)
 
 ####connection####
 
-connection <- dbConnect(odbc::odbc(),
-                        Driver = "MySQL", 
-                        #.connection_string = "Driver={MySQL ODBC 8.0 Unicode Driver};",
-                        Server="204.2.195.88", Database="mouse_phenotype_database",
-                        UID = "guest_mpd_app",
-                        PWD="fuffu_2021_!?_sambapati", Port=17703)
+connection <- tryCatch({dbConnect(odbc::odbc(),
+                                  Driver = "MySQL", 
+                                  Server="204.2.195.88", Database="mouse_phenotype_database",
+                                  UID = "guest_mpd_app",
+                                  PWD="fuffu_2021_!?_sambapati", Port=17703)},
+                       
+                       error = function(e)
+                       
+                      {dbConnect(odbc::odbc(),
+                                  .connection_string = "Driver={MySQL ODBC 8.0 Unicode Driver};",
+                                  Server="204.2.195.88", Database="mouse_phenotype_database",
+                                  UID = "guest_mpd_app",
+                                  PWD="fuffu_2021_!?_sambapati", Port=17703)})
 
 
 ####Server####
